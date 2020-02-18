000c7ad3e3bd107802257d926290138aebb1057f
olivier beyler <olivier.beyler@orange.com>
2020-02-18 10:55:42 +0100

    Add pause image

--- addblob.sh
+++ addblob.sh
@@ -2,3 +2,4 @@ export containerd_version=1.3.3
 curl -L https://storage.googleapis.com/cri-containerd-release/cri-containerd-${containerd_version}.linux-amd64.tar.gz  --output containerd-${containerd_version}.linux-amd64.tar.gz
 bosh add-blob containerd-${containerd_version}.linux-amd64.tar.gz /containerd/containerd.linux-amd64.tar.gz
 rm containerd-${containerd_version}.linux-amd64.tar.gz
+bosh add-blob pause.tar /containerd/pause.tar
